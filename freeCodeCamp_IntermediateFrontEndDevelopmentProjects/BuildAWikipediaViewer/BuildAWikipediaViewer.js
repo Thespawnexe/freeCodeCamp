7195b90c4b98282cb4ab0f31695d901e1511d284
Thespawnexe <manycrazy2040@yahoo.com>
2017-08-18 15:39:31 -0700

    Adding templates and starting work on wiki viewer.
    - Added html template for the Wiki Viewer and Twitch tv projects.
    - Added possible Wiki viewer color template (I might change it).
    - Added the project title, the input and button for wiki search, random wiki article link and my codepen link/name.

---
+++ freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
@@ -0,0 +1,11 @@
+/*
+useful links to use for project.
+-  a URL you can use to get a random Wikipedia article: https://en.wikipedia.org/wiki/Special:Random
+-  Here's an entry on using Wikipedia's API: https://www.mediawiki.org/wiki/API:Main_page
+-  Use this link to experiment with Wikipedia's API: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm
+*/
+
+
+$(document).ready(function(){
+    $('.wiki-section').html("hows this for a test of fun");
+});
