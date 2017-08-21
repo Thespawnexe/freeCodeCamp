656bc98bef8bc485462a57e31bad20e0aa3bb0b6
Thespawnexe <manycrazy2040@yahoo.com>
2017-08-21 00:37:32 -0700

    Button and search bar work
    - Stylized buttons and search bar.
    - Starting work on getting input to save so I can use it for search queries.

--- freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
+++ freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
@@ -5,7 +5,12 @@ useful links to use for project.
 -  Use this link to experiment with Wikipedia's API: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm
 */
 
+ 
 
 $(document).ready(function(){
-    $('.wiki-section').html("hows this for a test of fun");
+    var input = document.getElementById("wiki-search").value;
+    $( "#search-button" ).click(function() {
+        $( ".wiki-section" ).html(input);
+    });
+    
 });
