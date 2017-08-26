121233cdef64c51cee0d6f8c6906dfb879d2f326
Thespawnexe <manycrazy2040@yahoo.com>
2017-08-26 15:06:52 -0700

    wiki image and working query search.
    I can now search for something and print it to the page.
    
    -added a wiki globe img that i may remove.
    -made the search term the list's 'title'
    - made the search term spit out 10 search results and currently have it set to print two test ones so I can format and style them.

--- freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
+++ freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
@@ -3,6 +3,7 @@ useful links to use for project.
 -  a URL you can use to get a random Wikipedia article: https://en.wikipedia.org/wiki/Special:Random
 -  Here's an entry on using Wikipedia's API: https://www.mediawiki.org/wiki/API:Main_page
 -  Use this link to experiment with Wikipedia's API: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm
+- help video: https://youtu.be/_7zRKUz-G3s?t=464
 */
 
  
@@ -20,13 +21,24 @@ $(document).ready(function(){
     getSearchTerm.addEventListener("click", findWikiPage, false);
 
     function findWikiPage() {
-        var searchTerm = document.getElementById("wiki-search").value;
+        var searchTerm = "doppler";//document.getElementById("wiki-search").value;
         var printSearchResult = document.getElementById("wiki-section");
-        var wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&grnnamespace=0&prop=extracts&exlimit=max&explaintext&exintro&gsrsearch=" + searchTerm + "&callback=?";
-        //var wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions|images|info&titles=cookie&rvprop=content&imlimit=1&callback=?";
+        var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm +  "&callback=?"; //this works
+
         $.getJSON( wikiUrl, function(data) {
-            var result= data.query.pages[0].extract;
-            $(printSearchResult).html(result);
+            var title = data[0];
+            var topic = data[1];
+            var topicDescription = data[2];
+            var topicUrl = data[3];
+            $("#wiki-search-title").html(title.toUpperCase());
+            
+            $("ul").html(
+                "<li>" + topic[9] + "<a href="+ topicUrl[9] + " target='_blank'>link</a> " + topicDescription[9] + "</li>" + 
+                "<li>" + topic[8] + "<a href="+ topicUrl[8] + " target='_blank'>link</a> " + topicDescription[8] + "</li>"
+            
+            );
+            
+            //$(printSearchResult).html(result);
         });
 
     }    
