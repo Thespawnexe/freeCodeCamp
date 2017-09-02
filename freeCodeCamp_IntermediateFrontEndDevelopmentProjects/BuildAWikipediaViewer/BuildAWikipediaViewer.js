43a46a4b5396c4397cc5a195d20880802d1fb5ac
Thespawnexe <manycrazy2040@yahoo.com>
2017-09-02 10:56:32 -0700

    Lots of changes and mostly done.
    - Changed color scheme.
    - Added fonts, borders, shadows and an image.
    - Added some jquery animations
    - Fixed cases where there was no search term or no search term results are found. It now tells you to try another search.

--- freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
+++ freeCodeCamp_IntermediateFrontEndDevelopmentProjects/BuildAWikipediaViewer/BuildAWikipediaViewer.js
@@ -1,13 +1,3 @@
-/*
-useful links to use for project.
--  a URL you can use to get a random Wikipedia article: https://en.wikipedia.org/wiki/Special:Random
--  Here's an entry on using Wikipedia's API: https://www.mediawiki.org/wiki/API:Main_page
--  Use this link to experiment with Wikipedia's API: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm
-- help video: https://youtu.be/_7zRKUz-G3s?t=464
-*/
-
- 
-
 $(document).ready(function(){
     var getSearchTerm = document.getElementById("search-button"); 
    
@@ -24,26 +14,31 @@ $(document).ready(function(){
         var searchTerm = document.getElementById("wiki-search").value;
        // var printSearchResult = document.getElementById("wiki-section");
         var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=5&search=" + searchTerm +  "&callback=?"; //this works
-
+        
         if ( searchTerm.length === 0) {
-            $("ul").html("<li>No search results found. Please try another search.</li>");
+            $("#wiki-search-title").html("<p style='font-size: 32px'>No search results found. Please type in a search term to continue.</p>");
+            $("ul").empty();
         } else {
+            $("ul").hide("fade", "fast");    
             $.getJSON( wikiUrl, function(data) {
                 var title = data[0];
                 var topic = data[1];
                 var topicDescription = data[2];
                 var topicUrl = data[3];
                 
-                if (topic.length === 0) {
-                    $("ul").html("<li>No search results found. Please try another search.</li>");    
+                if (topicDescription.length < 1) {
+                    $("#wiki-search-title").html("<p style='font-size: 32px'>No search results found. Please try another search.</p>");   
                 } else {
-                    $("ul").empty();
+                   // $("ul").empty();
                     $("#wiki-search-title").html(title.toUpperCase());
-                    for(var i=0; i < topic.length; i++) { 
+                    
+                    for(var i=0; i < topic.length; i++) {
+                         
                         $("ul").append(
-                        "<li><div class='topic-title'>" + topic[i] + "</div><div class='topic-description'>" + topicDescription[i] + "</div><div><a href="+ topicUrl[i] + " target='_blank' class='text-right'>Find out more...</a></div></li>" 
-                        );
+                        "<li class='box-shadow'><div class='topic-title'>" + topic[i] + "</div><div class='topic-description'>" + topicDescription[i] + "</div><div><a href="+ topicUrl[i] + " target='_blank' id='topic-url'>Find out more...</a></div></li>" 
+                        );    
                     }
+                    $("ul").show("fade", "fast");
                 }
             });
         }
